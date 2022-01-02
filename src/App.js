e43c292062cb9591723fa350babdb3d07614e5d3
Derek <derekcrosslu@gmail.com>
2022-01-02 10:34:20 -0500

    parsing response object into link and label elements

--- src/App.js
+++ src/App.js
@@ -4,15 +4,23 @@ import { parse } from 'postcss';
 import React, { useState, useEffect } from 'react'
 
 function App() {
+  const [data, setData] = useState([])
   useEffect(() => {
     const loadData = async () => {
       const response = await axios.get(`https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&search=Goo`)
-
-      console.log(response, 'response');
+      const parseResponse = []
+      for (let i = 0; i < response.data[1].length; i++) {
+        parseResponse.push({
+          id: response.data[3][1],
+          label: response.data[1][i]
+        })
+      }
+      setData(parseResponse)
 
     }
     loadData()
   }, [])
+  console.log(data, 'data');
   return (
     <div className="App">
       <h1 className="text-3xl font-bold underline">
