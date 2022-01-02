52ece83ca349b231ece5f27f3abb00e6caa44c3f
Derek <derekcrosslu@gmail.com>
2022-01-02 10:29:11 -0500

    using axios to get response object from wikipedia

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import './App.css';
+import axios from 'axios'
+import { parse } from 'postcss';
+import React, { useState, useEffect } from 'react'
 
 function App() {
+  useEffect(() => {
+    const loadData = async () => {
+      const response = await axios.get(`https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&search=Goo`)
+
+      console.log(response, 'response');
+
+    }
+    loadData()
+  }, [])
   return (
     <div className="App">
       <h1 className="text-3xl font-bold underline">
